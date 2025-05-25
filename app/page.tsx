35ce29483764703aa993c030a3ace97a3bec94e0
Georgiana Blue <georgianablue@gmail.com>
2025-05-25 14:57:58 -0400

    Import ToDoListComponent to the app home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import ToDoListComponent from "./components/todo-list-component";
+
 export default function ToDoHome() {
   return (
     <div className="flex flex-col items-center justify-start  min-h-screen p-4">
       <h1 className="font-bold">To Do List</h1>
       <p>Welcome to your To Do List!</p>
+      <ToDoListComponent />
     </div>
   );
 }
