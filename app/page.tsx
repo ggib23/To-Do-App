e3e505f3a50de3e89918ef37c3457db9f106511f
Georgiana Blue <georgianablue@gmail.com>
2025-05-26 15:36:59 -0400

    Create client side request to get tasks

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import ToDoListComponent from "./components/todo-list-component";
 
-export default function ToDoHome() {
+export default async function ToDoHome() {
+  // Call the /tasks API route to fetch the tasks
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/tasks`, { cache: "no-store" });
+  const tasks = await res.json();
+
   return (
     <div className="flex flex-col items-center justify-start  min-h-screen p-4">
       <h1 className="font-bold">To Do List</h1>
       <p>Welcome to your To Do List!</p>
-      <ToDoListComponent />
+      <ToDoListComponent tasks={tasks}/>
     </div>
   );
 }
